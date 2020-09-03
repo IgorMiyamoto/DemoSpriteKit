740a0dec490d1f86e1345859745df35eb747451f
Igor Kenzo <36746677+IgorKenzo@users.noreply.github.com>
2020-09-03 18:18:21 -0300

    gamescene att

--- DemoSpriteKit/DemoSpriteKit/GameScene.swift
+++ DemoSpriteKit/DemoSpriteKit/GameScene.swift
@@ -8,16 +8,37 @@
 
 import SpriteKit
 import GameplayKit
+import AVFoundation
 
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
     //Variável para a action do som
     var contato : SKAction!
+    private var BGM = AVAudioPlayer()
     
     //Função chamada quando a Scene é carregada
     override func didMove(to view: SKView) {
         //Define o delegate de contato
         physicsWorld.contactDelegate = self
+        
+        
+        // Cria uma constante com a caminho do arquivo de audio no projeto.
+        let BGMPath = Bundle.main.url(forResource: "grilos", withExtension: "wav")
+        
+        // Tenta carregar o arquivo no player.
+        do {
+            BGM = try AVAudioPlayer(contentsOf: BGMPath!, fileTypeHint: "wav")
+        }
+        // Se falhar, imprime o erro no console.
+        catch {
+            print(error)
+        }
+        
+        // Define quantas vezes a música deve tocar, utilize qualquer valor negativo para configurar um loop infinito.
+        BGM.numberOfLoops = -1
+        // Começa a tocar a música.
+        BGM.play()
+        
         
         //Instancio a SKAction
         contato = SKAction.playSoundFileNamed("Hit.wav", waitForCompletion: false)
